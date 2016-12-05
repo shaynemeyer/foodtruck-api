38b66e528cf03f604bab6efd995541a25a4fdcd5
Shayne Meyer <shayne.meyer@gmail.com>
2016-12-05 06:17:05 -0800

    updated postman and added a seed data file

---
+++ postman/seed-data.js
@@ -0,0 +1,24 @@
+{
+	"name": "Joe's Stabbin Wagon",
+	"foodtype": "Meat",
+	"avgcost": 5.99,
+	"geometry": {"coordinates": [35.89, -89.324]}
+}
+
+{
+	"name": "Pam's Pancake Shack",
+	"foodtype": "Breakfast",
+	"avgcost": 7.99,
+	"geometry": {"coordinates": [33.89, -93.324]}
+}
+
+// reviews 
+{
+	"title": "Pretty not bad!",
+	"text": "A little expensive but the food was pretty good."
+}
+
+{
+	"title": "Just ok",
+	"text": "Nothing to write home about."
+}
