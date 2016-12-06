e387a3a60c2a37a715b3e0e489d095845bc8c36a
Shayne Meyer <shayne.meyer@gmail.com>
2016-12-05 19:25:25 -0800

    added authentication with passport

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import config from '../config';
 import middleware from '../middleware';
 import initializeDb from '../db';
 import foodtruck from '../controller/foodtruck';
+import account from '../controller/account';
 
 let router = express();
 
@@ -14,7 +15,7 @@ initializeDb(db => {
 
   // api routes v1 (/v1)
   router.use('/foodtruck', foodtruck({ config, db }));
-
+  router.use('/account', account({ config, db }));
 });
 
 export default router;
